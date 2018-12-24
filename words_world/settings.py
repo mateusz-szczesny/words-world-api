c91c8b792efa8823a8ed0b95c386925bcc8c1f4a
Mateusz <matty.szczesny@gmail.com>
2018-12-24 11:28:06 +0100

    add dokku env settings

--- words_world/settings.py
+++ words_world/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!al3ccb4p7u0r6gfe$lj_mif37^nn$oznzx0j2w@h=8b#07xyh'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -134,5 +131,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
